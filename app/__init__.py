43e14071a246a9fa9712de5ac0cb8fa6e4578414
PrabhuKeDarshan <muralidharaprabhu@gmail.com>
2017-03-20 09:55:39 +0300

    with valid login & logout settings from OpenId

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,26 @@
+import os
+
+from flask_login import LoginManager
+
+from flask_openid import OpenID
+from config import basedir
+
 from flask import Flask
+
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config.from_object('config')
 
-from app import views
+db = SQLAlchemy(app)
+
+lm = LoginManager()
+
+lm.init_app(app)
+
+lm.login_view = 'login'
+
+oid = OpenID(app,os.path.join(basedir, 'tmp'))
+
+from app import views, models
