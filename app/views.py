eaf7acd792ea9b1c106743959f026793ce9348ae
PrabhuKeDarshan <muralidharaprabhu@gmail.com>
2017-03-27 13:47:46 +0300

    restarting project

--- app/views.py
+++ app/views.py
@@ -28,6 +28,18 @@ def login():
                            form=form,
                            providers = app.config['OPENID_PROVIDERS'])
 
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
+
 @oid.after_login
 def after_login(resp):
     if resp.email is None or resp.email == "":
@@ -38,6 +50,7 @@ def after_login(resp):
         nickname = resp.nickname
         if nickname is None or nickname == "":
             nickname = resp.email.split('@')[0]
+        nickname = User.make_unique_nickname(nickname)
         user = User(nickname=nickname, email=resp.email)
         db.session.add(user)
         db.session.commit()
@@ -95,7 +108,7 @@ def user(nickname):
 @app.route('/edit', methods=['GET', 'POST'])
 @login_required
 def edit():
-    form = EditForm()
+    form = EditForm(g.user.nickname)
     if form.validate_on_submit():
         g.user.nickname = form.nickname.data
         g.user.about_me = form.about_me.data
